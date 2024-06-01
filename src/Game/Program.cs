cc7ca06bd1088c0cb16dadc09629596eced811a4
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2024-06-01 13:37:28 -0400

    basic engine working

--- src/Game/Program.cs
+++ src/Game/Program.cs
@@ -1,4 +1,10 @@
-﻿Engine.Run(new Engine.RunOptions(1920,1080,"zinc", () => {
-        new Shape();
+﻿using Zinc;
+
+Engine.Run(new Engine.RunOptions(1920,1080,"zinc", () => {
+        new Shape(new Color(Palettes.ENDESGA[9]))
+        {
+            X = Engine.Width / 2f,
+            Y = Engine.Height / 2f,
+        };
     }
 ));
