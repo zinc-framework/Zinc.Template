9ff97202948781707735115470acc7b9ee2b0655
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2025-08-15 23:17:07 -0400

    specificy color parameter

--- src/Game/Program.cs
+++ src/Game/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Zinc;
 
 Engine.Run(new Engine.RunOptions(1920,1080,"zinc", () => {
-        new Shape(new Color(Palettes.ENDESGA[9]))
+        new Shape(color : new Color(Palettes.ENDESGA[9]))
         {
             X = Engine.Width / 2f,
             Y = Engine.Height / 2f,
